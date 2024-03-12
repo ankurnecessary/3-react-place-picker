d97a242aa368977e4b994e0a4618a458f47b5631
Ankur Gupta <ankurnecessary@gmail.com>
2024-03-13 03:11:22 +1030

    Adding timer in delete confirmation. Now it will auto confirm itself after 3 seconds if user is not able to take a decision in that time.

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,4 +1,25 @@
-export default function DeleteConfirmation({ onConfirm, onCancel }) {
+import React from 'react';
+import {useEffect} from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ * A custom React component that displays delete message in modal window.
+ *
+ * @component
+ * @param {object} props - The props object containing component's properties.
+ * @return {JSX.Element} A JSX element representing Delete confirmation message.
+ */
+function DeleteConfirmation({onConfirm, onCancel}) {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onConfirm();
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
@@ -14,3 +35,10 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
     </div>
   );
 }
+
+DeleteConfirmation.propTypes = {
+  onConfirm: PropTypes.func.isRequired,
+  onCancel: PropTypes.func.isRequired,
+};
+
+export default DeleteConfirmation;
